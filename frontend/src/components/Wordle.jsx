ea03238b75022b8bba1dd09172ae4b6d3e153b2a
JVanderstoop <J.vanderstoop@outlook.com>
2022-02-17 16:17:40 -0700

    Added button functionality

--- frontend/src/components/Wordle.jsx
+++ frontend/src/components/Wordle.jsx
@@ -4,6 +4,14 @@ import {FaBackspace} from 'react-icons/fa';
 function Wordle(){
 
     var words = new Array(30).fill(0);
+    var guessPosition = 0; 
+
+    function addLetter(a)
+    {
+        guessPosition++; 
+        words[guessPosition] = a; 
+        alert(words[guessPosition] +  ' guessPosition: ' + guessPosition);
+    }
        
 
    
@@ -12,7 +20,7 @@ function Wordle(){
             <h1 className="game-name">Wordle</h1>
 
             {/*Real version creates game tiles and game rows to import and create this */}
-            <ul className="geusses">
+            <ul className="guesses">
             {words.map(function(words){
                         return <li className='letter'>
                             
@@ -25,42 +33,42 @@ function Wordle(){
             <div id="keyboard">
 
                 <div className='row'>
-                    <button data-key='q'>q</button>
-                    <button data-key='w'>w</button>
-                    <button data-key='e'>e</button>
-                    <button data-key='r'>r</button>
-                    <button data-key='t'>t</button>
-                    <button data-key='y'>y</button>
-                    <button data-key='u'>u</button>
-                    <button data-key='i'>i</button>
-                    <button data-key='o'>o</button>
-                    <button data-key='p'>p</button>
+                    <button onClick= { () => addLetter('q') } data-key='q'>q</button>
+                    <button onClick= { () => addLetter('w') }data-key='w'>w</button>
+                    <button onClick= { () => addLetter('e') }data-key='e'>e</button>
+                    <button onClick= { () => addLetter('r') }data-key='r'>r</button>
+                    <button onClick= { () => addLetter('t') }data-key='t'>t</button>
+                    <button onClick= { () => addLetter('y') }data-key='y'>y</button>
+                    <button onClick= { () => addLetter('u') }data-key='u'>u</button>
+                    <button onClick= { () => addLetter('i') }data-key='i'>i</button>
+                    <button onClick= { () => addLetter('o') }data-key='o'>o</button>
+                    <button onClick= { () => addLetter('p') }data-key='p'>p</button>
 
                 </div>
                 
                 <div className='row'>
-                    <button data-key='a'>a</button>
-                    <button data-key='s'>s</button>
-                    <button data-key='d'>d</button>
-                    <button data-key='f'>f</button>
-                    <button data-key='g'>g</button>
-                    <button data-key='h'>h</button>
-                    <button data-key='j'>j</button>
-                    <button data-key='k'>k</button>
-                    <button data-key='l'>l</button>
+                    <button onClick= { () => addLetter('a') } data-key='a'>a</button>
+                    <button onClick= { () => addLetter('s') } data-key='s'>s</button>
+                    <button onClick= { () => addLetter('d') } data-key='d'>d</button>
+                    <button onClick= { () => addLetter('f') } data-key='f'>f</button>
+                    <button onClick= { () => addLetter('g') } data-key='g'>g</button>
+                    <button onClick= { () => addLetter('h') } data-key='h'>h</button>
+                    <button onClick= { () => addLetter('j') } data-key='j'>j</button>
+                    <button onClick= { () => addLetter('k') } data-key='k'>k</button>
+                    <button onClick= { () => addLetter('l') } data-key='l'>l</button>
                     
                 </div>
                 
                 <div className='row'>
-                    <button data-key='↵' className='one-and-a-half'>enter</button>
-                    <button data-key='z'>z</button>
-                    <button data-key='x'>x</button>
-                    <button data-key='c'>c</button>
-                    <button data-key='v'>v</button>
-                    <button data-key='b'>b</button>
-                    <button data-key='n'>n</button>
-                    <button data-key='m'>m</button>
-                    <button data-key='←' className='one-and-a-half'><FaBackspace size={20} /></button>
+                    <button onClick= { () => addLetter('enter') } data-key='↵' className='one-and-a-half'>enter</button>
+                    <button onClick= { () => addLetter('z') } data-key='z'>z</button>
+                    <button onClick= { () => addLetter('x') } data-key='x'>x</button>
+                    <button onClick= { () => addLetter('c') } data-key='c'>c</button>
+                    <button onClick= { () => addLetter('v') } data-key='v'>v</button>
+                    <button onClick= { () => addLetter('b') } data-key='b'>b</button>
+                    <button onClick= { () => addLetter('n') } data-key='n'>n</button>
+                    <button onClick= { () => addLetter('m') } data-key='m'>m</button>
+                    <button onClick= { () => addLetter('backspace') } data-key='←' className='one-and-a-half'><FaBackspace size={20} /></button>
                 </div>
             </div>
                 

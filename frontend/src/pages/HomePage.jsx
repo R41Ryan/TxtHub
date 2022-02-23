ec03f525859c061a41572fa2b56b8dae80a775f1
JVanderstoop <J.vanderstoop@outlook.com>
2022-02-22 19:27:46 -0700

    Merge github.com:Hahkobeh/TxtHub

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -15,6 +15,15 @@ function HomePage(){
         
         
     }
+
+    function anagramsHandler(){
+
+        navigate('/anagrams');
+        
+        
+    }
+
+
     return(
         <div className='games-page'>
             <Layout/>
@@ -24,6 +33,9 @@ function HomePage(){
                     <li className='game'>
                         <div className='card' onClick={wordleHandler}>
                             <h1>Play Wordle!</h1>
+                        </div>
+                        <div className='card' onClick={anagramsHandler}>
+                            <h1>Play Anagrams!</h1>
                         </div>
                     </li>
                 </ul>
